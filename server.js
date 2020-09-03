962e1adac882c576b37d77ea71a3cc2be7b0eb86
Muhsar <Jewbreel1@gmail.com>
2020-09-04 00:03:23 +0100

    -

--- server.js
+++ server.js
@@ -23,15 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/farmer', farmerRouter);
 app.use('/consumer', consumerRouter)
-if(process.env.NODE_ENV==='production'){
-  app.use(express.static('client/build'))
-  app.get('*', (req,res)=>{
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-  })
-}
-
 const port = process.env.PORT || 5000
-
 app.listen(port, () => {
   console.log(`Server Running at ${port}`)
 });
